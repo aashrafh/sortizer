a5eed30e906aba7eecd839c705db19545f6e531f
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2020-11-06 19:29:41 +0200

    Implement Insertion Sort

---
+++ src/InsertionSort.hpp
@@ -0,0 +1,34 @@
+#ifndef INSERTION_SORT_H
+#define INSERTION_SORT_H
+#include "./Sortizer.hpp"
+class InsertionSort: public Sortizer
+{
+protected:
+    void sortUtil(vector<long long> &list, int size);
+public:
+    InsertionSort();
+};
+
+InsertionSort::InsertionSort()
+{
+}
+/*
+Stable: Yes
+In place: Yes
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+Adaptive: Yes. O(n) time when nearly sorted
+*/
+void InsertionSort::sortUtil(vector<long long> &list, int size){
+    for(int idx = 1; idx < size; idx++){
+        long long key = list[idx];
+        // Insert list[idx] into the sorted sequence list[0..idx-1]
+        int it = idx-1;
+        while(it > 0 && list[it] > key){
+            list[it+1] = list[it];
+            it--;
+        }
+        list[it+1] = key;
+    }
+}
+#endif
