cdec7f5c43cb8c14ae19b966a405a5a2b88ad2d5
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2020-11-07 02:14:54 +0200

    Automate generating the random input

--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 g++ -O2 sort.cpp
 
+if [ ! -d ../input ]
+then
+    mkdir ../input
+fi
 if [ ! -d ../sorted ]
 then
     mkdir ../sorted
@@ -10,15 +14,14 @@ then
     mkdir ../stats
 fi
 
+for i in 100 1000 5000 10000 50000 75000 100000 500000; do
+    python generate.py "$i" "../input/data$i.txt"
+done
+
+
 for filepath in ../input/*.txt; do
     filename=$(basename $filepath .txt)
-    # echo $filepath
-    # ./a.out 0 "$filepath" "../sorted/random-$filename-sorted-0.txt" "../stats/random-$filename-stats-0.txt"
-    #0 "./input/data1000.txt" "../sorted/random-data1000-sorted-0.txt" "../stats/random-data1000-stats-0.txt"
-    # echo "$filename"\
-    # echo $filepath
     for ((i=0; i<=3; i++)); do
         ./a.out "$i" "$filepath" "../sorted/random-$filename-sorted-$i.txt" "../stats/random-$filename-stats-$i.txt"
-        # echo "../sorted/random-$filename-stats-$i.txt"
     done
 done
