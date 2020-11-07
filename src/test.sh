c05c20589ee399f59baebb1d221b1d38dc6741d3
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2020-11-07 14:28:08 +0200

    Test against random and sorted data

--- src/test.sh
+++ src/test.sh
@@ -1,27 +1,34 @@
 #!/bin/sh
 g++ -O2 sort.cpp
 
-if [ ! -d ../input ]
+if [ -d ../random ]
 then
-    mkdir ../input
+    rm -r ../random
 fi
-if [ ! -d ../sorted ]
+if [ -d ../sorted ]
 then
-    mkdir ../sorted
+    rm -r ../sorted
 fi
-if [ ! -d ../stats ]
-then
-    mkdir ../stats
-fi
+mkdir ../random ../sorted
+mkdir ../random/input ../random/output ../random/stats
+mkdir ../sorted/input ../sorted/output ../sorted/stats
 
 for i in 100 1000 5000 10000 50000 75000 100000 500000; do
-    python generate.py "$i" "../input/data$i.txt"
+    python generate.py "$i" "../random/input/data$i.txt"
 done
 
 
-for filepath in ../input/*.txt; do
+for filepath in ../random/input/*.txt; do
     filename=$(basename $filepath .txt)
     for ((i=0; i<=3; i++)); do
-        ./a.out "$i" "$filepath" "../sorted/random-$filename-sorted-$i.txt" "../stats/random-$filename-stats-$i.txt"
+        ./a.out "$i" "$filepath" "../random/output/random-$filename-sorted-$i.txt" "../random/stats/random-$filename-stats-$i.txt"
+    done
+    cp ../random/output/random-$filename-sorted-1.txt ../sorted/input/
+done
+
+for filepath in ../sorted/input/*.txt; do
+    filename=$(basename $filepath .txt)
+    for ((i=0; i<=3; i++)); do
+        ./a.out "$i" "$filepath" "../sorted/output/random-$filename-sorted-$i.txt" "../sorted/stats/random-$filename-stats-$i.txt"
     done
 done
